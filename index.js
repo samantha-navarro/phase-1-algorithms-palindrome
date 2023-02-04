785c30aa4563655805015bac224688dea67eff5d
Samantha Navarro <samantha.m.navarro@gmail.com>
2023-02-04 15:28:03 -0800

    commit tree 238da9d5afddc03e99f887b9dba8e29aac56d6dd

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
+function reverseString(word) {
+  const wordArray = word.split("");
+  const reversedWordArray = wordArray.reverse("");
+  const reversedWord = reversedWordArray.join("");
+  return reversedWord;
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverseString(word);
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
 */
 
+// need an if else statement
+//
 /*
   Add written explanation of your solution here
 */
